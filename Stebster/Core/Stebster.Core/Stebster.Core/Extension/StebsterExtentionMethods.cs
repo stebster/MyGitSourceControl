0fb5fb883de1ba0ffc2724e99869aa0b6115cfe1
Stebster <stephenbridge@tinyonline.co.uk>
2015-08-02 16:30:26 +0100

    Tidy extension methods an tests

--- Stebster/Core/Stebster.Core/Stebster.Core/Extension/StebsterExtentionMethods.cs
+++ Stebster/Core/Stebster.Core/Stebster.Core/Extension/StebsterExtentionMethods.cs
@@ -12,18 +12,28 @@
         /// </summary>
         /// <typeparam name="T">Class type of the passed object</typeparam>
         /// <param name="obj">The object iteself</param>
+        /// <param name="prefix">Optional namespace prefix</param>
+        /// <param name="ns">Optional namespace</param>
         /// <returns>The XElement that the object serialises to</returns>
-        public static XElement ToXElement<T>(this object obj)
+        public static XElement ToXElement<T>(this object obj, string prefix="", string ns="")
         {
             XElement element;
 
-            using (var memoryStream = new MemoryStream())
+            using (var stream = new MemoryStream())
             {
-                using (TextWriter streamWriter = new StreamWriter(memoryStream))
+                using (var streamWriter = new StreamWriter(stream))
                 {
-                    var xmlSerializer = new XmlSerializer(typeof(T));
-                    xmlSerializer.Serialize(streamWriter, obj);
-                    element = XElement.Parse(Encoding.ASCII.GetString(memoryStream.ToArray()));
+                    // Create list of namespaces for serialiser and add the optional passed ones
+                    var serialiserNamespaces = new XmlSerializerNamespaces();
+                    serialiserNamespaces.Add(prefix, ns);
+
+                    // Serialise passed object to memory stream
+                    var serializer = new XmlSerializer(typeof(T));
+                    serializer.Serialize(streamWriter, obj, serialiserNamespaces);
+
+                    // Get the string from the memory stream and parse as an Xelement
+                    var xmlString = Encoding.UTF8.GetString(stream.ToArray());
+                    element = XElement.Parse(xmlString);
                 }
             }
 
@@ -42,11 +52,11 @@
 
             using (var xmlReader = xElement.CreateReader())
             {
-                var xmlSerializer = new XmlSerializer(typeof(T));
+                var serializer = new XmlSerializer(typeof(T));
 
-                if (xmlSerializer.CanDeserialize(xmlReader))
+                if (serializer.CanDeserialize(xmlReader))
                 {
-                    obj = xmlSerializer.Deserialize(xmlReader) as T;
+                    obj = serializer.Deserialize(xmlReader) as T;
                 }
             }
            
